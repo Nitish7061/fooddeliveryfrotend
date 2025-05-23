89756133214bef334053b5984b2c8dc2e32bcc4e
Nitish7061 <your-email@example.com>
2025-05-23 13:40:17 +0530

    Add all project files

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here </h2>
+            <p>Chosse from a drives menu featuring a dellectable array of dishes crafted with finsed </p>
+            <button>View Menu</button>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
